6387e83cb08b614caafbf7729b1cb5a3d8be2ebf
MrWhiteRice <jacob_kotro@hotmail.com>
2019-06-03 14:00:18 +1000

    project replace

---
+++ Assets/Scripts/Base Character Properties/IsometricCharacterRenderer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricCharacterRenderer : MonoBehaviour
+{
+
+    public static readonly string[] idleDirections = { "Idle N", "Idle NE", "Idle E", "Idle SE", "Idle S", "Idle SW", "Idle W", "Idle NW" };
+    public static readonly string[] runDirections = { "Run N", "Run NE", "Run E", "Run SE", "Run S", "Run SW", "Run W", "Run NW" };
+
+    public GameObject characterImageObject;
+    Animator anim;
+    int lastDirection;
+
+    private void Awake()
+    {
+        anim = characterImageObject.GetComponent<Animator>();
+    }
+
+
+    public void SetDirection(Vector2 direction)
+    {
+        string[] directionArray = null;
+
+        if (direction.magnitude < .01f)
+        {
+            directionArray = idleDirections;
+        }
+        else
+        {
+            directionArray = runDirections;
+            lastDirection = DirectionToIndex(direction, 8);
+        }
+        anim.Play(directionArray[lastDirection]);
+    }
+
+    public static int DirectionToIndex(Vector2 dir, int sliceCount)
+    {
+        Vector2 normDir = dir.normalized;
+        float step = 360f / sliceCount;
+        float halfstep = step / 2;
+        float angle = Vector2.SignedAngle(Vector2.up, normDir);
+        angle += halfstep;
+
+        if (angle < 0)
+        {
+            angle += 360;
+        }
+
+        float stepCount = angle / step;
+
+        return Mathf.FloorToInt(stepCount);
+        
+    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
---
+++ Assets/Scripts/Base Character Properties/IsometricPlayerMovementController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricPlayerMovementController : MonoBehaviour
+{
+    public int playerNumber; //this will need to be accessed on other scripts, such as stats and what not
+
+    //controlls
+    private string[] horizontalArray = new string[4] { "P1Horizontal", "P2Horizontal", "P3Horizontal", "P4Horizontal" };
+    private string[] verticalArray = new string[4] { "P1Vertical", "P2Vertical", "P3Vertical", "P4Vertical" };
+    private string[] basicAbilityArray = new string[4] { "P1BasicAbility", "P2BasicAbility", "P3BasicAbility", "P4BasicAbility" }; //when making inheritance and stats and all, move these to them
+    private string[] movementAbilityArray = new string[4] { "P1MovementAbility", "P2MovementAbility", "P3MovementAbility", "P4MovementAbility" };
+
+    private int basicAbilityKey = 0;
+    public float baseMovementSpeed = 1f;
+    private float currentSpeed;
+
+    Rigidbody2D rbody;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //}
+
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        currentSpeed = baseMovementSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        MovementFunction(playerNumber);
+        BasicTest(playerNumber);
+        MovementAbilityTest(playerNumber);
+    }
+
+    private void MovementFunction(int playerNum)
+    {
+        Vector2 currentPos = rbody.position;
+        float horizontalInput = Input.GetAxis(horizontalArray[playerNum]);
+        float verticalInput = Input.GetAxis(verticalArray[playerNum]);
+
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1); //1 might have to be baseMovementSpeed instead. clamp prevents diagonal movement being faster
+        Vector2 movement = inputVector * currentSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        //render
+        rbody.MovePosition(newPos);
+    }
+
+    private void BasicTest(int playerNum)
+    {
+        //if (Input.GetButtonDown("joystick " + (playerNum + 1) + " button " + basicAbilityKey))
+        if (Input.GetButtonDown(basicAbilityArray[playerNum]))
+        {
+            print("P" + playerNum + ": Pressed Basic Ability = " + basicAbilityKey);
+            currentSpeed += 5;
+        }
+
+    }
+
+    private void MovementAbilityTest(int playerNum)
+    {
+
+        if (Input.GetButtonDown(movementAbilityArray[playerNum]))
+        {
+            print("P" + playerNum + ": Pressed Movement Ability");
+            currentSpeed -= 5;
+        }
+    }
+}
